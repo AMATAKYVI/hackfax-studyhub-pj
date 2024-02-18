559b10951c7dda0e5729241da4ab6f0a62c64991
AMATAKYVI <yviamatak123@gmail.com>
2024-02-18 11:32:36 -0500

    firebase issue

--- src/db/firebase.config.tsx
+++ src/db/firebase.config.tsx
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBADE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
+  apiKey: 'AIzaSyBsTnrILm9nU85GzvMzsgufaHrtCmwI0LY',
+  authDomain: 'hackfax-e62a7.firebaseapp.com',
+  projectId: 'hackfax-e62a7',
   storageBucket: 'hackfax-e62a7.appspot.com',
   messagingSenderId: '602914139092',
   appId: '1:602914139092:web:ed777eb986f3cccbf5c441',
 };
 
 // Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
 
 // db
