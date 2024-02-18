a150ae2fe105a33b5b5a0bcbce5dcd86bca062d7
AMATAKYVI <yviamatak123@gmail.com>
2024-02-18 15:11:03 -0500

    Refactor Notification component to use TypeScript interface

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+
 interface NotificationProps {
   initialData: any;
 }
-const Notification = ({ initialData }: any) => {
+
+const Notification = ({ initialData }: NotificationProps) => {
   const [showPopup, setShowPopup] = useState(false);
 
   const togglePopup = () => {
@@ -21,29 +23,26 @@ const Notification = ({ initialData }: any) => {
         onClick={togglePopup}
       />
       {showPopup && (
-        <div className="absolute w-[200px] top-full right-0 mt-2 p-2 bg-gray-100 shadow-md rounded-md">
+        <div className="absolute w-[200px] top-full right-0 mt-2 p-2 bg-gray-100 shadow-md rounded-md max-h-60 overflow-y-auto">
           {/* Popup content */}
-          {initialData?.map((data: any) => {
-            console.log(data);
-            return (
-              <div key={data.uid} className="mb-2 flex items-center gap-2">
-                <Image
-                  className="rounded-lg"
-                  src={data.img}
-                  alt="Profile"
-                  width={20}
-                  height={20}
-                />
-                <div>
-                  <p className="text-xs font-semibold text-gray-600 ">
-                    Session: {data.title}
-                  </p>
-                  <p className="text-xs text-gray-500">{data.description}</p>
-                </div>
-                <hr />
+          {initialData?.map((data: any) => (
+            <div key={data.uid} className="mb-2 flex items-center gap-2">
+              <Image
+                className="rounded-lg"
+                src={data.img}
+                alt="Profile"
+                width={20}
+                height={20}
+              />
+              <div>
+                <p className="text-xs font-semibold text-gray-600 ">
+                  Session: {data.title}
+                </p>
+                <p className="text-xs text-gray-500">{data.description}</p>
               </div>
-            );
-          })}
+              <hr />
+            </div>
+          ))}
         </div>
       )}
       <span className="bg-red-500 text-xs text-white absolute top-0 right-2 transform translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-4 h-4 rounded-full">
