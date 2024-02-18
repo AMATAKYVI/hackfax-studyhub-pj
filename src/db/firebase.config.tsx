b7cc9a7436972724e095e2afabee27436aeece15
AMATAKYVI <yviamatak123@gmail.com>
2024-02-18 11:06:00 -0500

    env

--- src/db/firebase.config.tsx
+++ src/db/firebase.config.tsx
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: 'AIzaSyBsTnrILm9nU85GzvMzsgufaHrtCmwI0LY',
-  authDomain: 'hackfax-e62a7.firebaseapp.com',
-  projectId: 'hackfax-e62a7',
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
   storageBucket: 'hackfax-e62a7.appspot.com',
   messagingSenderId: '602914139092',
   appId: '1:602914139092:web:ed777eb986f3cccbf5c441',
