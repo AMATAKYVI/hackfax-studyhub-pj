87f44012ec53b3585fce7cf65805f5b6329706e8
AMATAKYVI <yviamatak123@gmail.com>
2024-08-06 23:50:02 -0400

    chore: Update npm dependencies, including @vercel/analytics to version 1.3.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AuthProvider } from '@/libs/AuthContext';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AuthProvider>
         <Component {...pageProps} />
+        <Analytics />
       </AuthProvider>
     </>
   );
